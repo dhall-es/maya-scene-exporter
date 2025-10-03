9ad9c42fa4d08c500766bfa37badf544b014a592
dhall-es <dylan.hall777@escapestudios.ac.uk>
2025-10-03 19:46:27 +0100

    added separateComponentString

---
+++ scripts/salt.py
@@ -0,0 +1,47 @@
+import maya.cmds as cmds
+
+# commandPort -n "localhost:7001" -stp "mel" -echoOutput;
+
+def separateComponentString(componentString, groups = [1]):
+    """
+    separateComponentString takes in a maya component string (e.g. "pCube1.e[8]"), and separates the components using the specified groups.
+    \nGroup 1 is the object name (e.g. "pCube1")
+    \nGroup 2 is the type (e.g. "e")
+    \nGroup 3 is the range (e.g. "8")
+    \nNote that Group 3 returns an list of values, in case the component string uses a range (e.g. "[8:10]")
+
+    :param str componentString: A maya component string.
+    :param list groups: The groups to be separated.
+    :returns list: The input 'groups' list with each element replaced with its specified group
+    """
+
+    import re
+    separated = []
+
+    match = re.match(r'([^\.]+)\.([^\[]+)\[([^\]]+)', componentString)
+    if match:
+        for i in range(len(groups)):
+            if groups[i] != 3:
+                group = match.group(groups[i])
+                separated += [group]
+                continue
+            
+            matchRange = re.match(r'(\d+):(\d+)', match.group(3))
+            if (not matchRange):
+                group = [match.group(3)]
+                separated += [group]
+                continue
+            rangeStart = int(matchRange.group(1))
+            rangeEnd = int(matchRange.group(2)) + 1
+
+            group = list(range(rangeStart, rangeEnd))
+            separated += [group]
+    else:
+        separated = [""]
+    
+    return separated
+
+# selected = cmds.ls(selection = True)
+
+# edges = cmds.polyListComponentConversion(selected, fromFace = True, toEdge = True, internal = False)
+# print(edges)
