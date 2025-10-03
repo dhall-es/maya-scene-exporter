f4f1b1d85c18046682544b4b6e8ec37780e7771a
dhall-es <dylan.hall777@escapestudios.ac.uk>
2025-10-03 21:08:42 +0100

    fixed an issue with SeparateComponentString

--- scripts/salt.py
+++ scripts/salt.py
@@ -28,20 +28,45 @@ def separateComponentString(componentString, groups = [1]):
             
             matchRange = re.match(r'(\d+):(\d+)', match.group(3))
             if (not matchRange):
-                group = [match.group(3)]
+                group = int(match.group(3))
                 separated += [group]
                 continue
             rangeStart = int(matchRange.group(1))
             rangeEnd = int(matchRange.group(2)) + 1
 
             group = list(range(rangeStart, rangeEnd))
-            separated += [group]
+            separated += group
     else:
         separated = [""]
     
     return separated
 
 # selected = cmds.ls(selection = True)
+# if (selected == [""]):
+#     print("nothing selected")
 
-# edges = cmds.polyListComponentConversion(selected, fromFace = True, toEdge = True, internal = False)
-# print(edges)
+# internalEdges = cmds.polyListComponentConversion(selected, fromFace = True, toEdge = True, internal = True)
+# components = separateComponentString(internalEdges[0], [1, 2])
+# print(internalEdges)
+
+# internalEdgeIndices = []
+# for edge in internalEdges:
+#     internalEdgeIndices += separateComponentString(edge, [3])
+# print(internalEdgeIndices)
+
+# allEdges = cmds.polyListComponentConversion(selected, fromFace = True, toEdge = True, internal = False)
+
+# externalEdges = []
+# for edge in allEdges:
+
+#     index = separateComponentString(edge, [3])
+#     print(index)
+    
+#     for i in index:
+#         if (internalEdgeIndices.__contains__(index)):
+#             continue
+#         externalEdges += [f"{components[0]}.{components[1]}[{i}]"]
+
+# cmds.select(clear = True)
+# print(externalEdges)
+# cmds.select(externalEdges)
